349f5c5bd1d730ba0895b78ac76805ad05c751e4
Antti Pham <antti.pham@tuni.fi>
2023-11-26 11:15:31 +0200

    a

--- public_html/ohjelmointi/tilinsiirto.php
+++ public_html/ohjelmointi/tilinsiirto.php
@@ -5,9 +5,28 @@
     <title>Tilinsiirto</title>
 </head>
 <body>
-    <?php
-        die("Tilinsiirto ei ole vielä käytössä.");
-    ?>
     <h1>Tilinsiirto</h1>
+
+
+    <form action="tilinsiirto.php" method="post">
+        <label for="summa">Siirrettävä summa:</label>
+        <input type="number" name="summa" />
+        <label for="veloitettava">Veloitettavan tilinumero:</label>
+        <input type="text" name="veloitettava" />
+        <label for="saaja">Saajan tilinumero:</label>
+        <input type="text" name="saaja" />
+        <input type="submit" value="Siirrä">
+    </form>
 </body>
+<?php
+    die("Tietokantayhteyden luominen epäonnistui.");
+    session_start();
+
+    $y_tiedot = "dbname=nxanph user=nxanph password=ezd6XpnRIzkK9nq";
+    $yhteys = pg_connect($y_tiedot)
+        or die("Tietokantayhteyden luominen epäonnistui.");
+
+    // POST käsittelijä
+    // if ($_POST[""]
+?>
 </html>
